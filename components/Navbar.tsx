a4200eb8cab414d42281be3f60eb463ce7fee37f
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-02-05 01:10:11 +0200

    add hero section

---
+++ components/Navbar.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { Menu, Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "./ui/navigation-menu";
+
+export default function Navbar() {
+  const { setTheme } = useTheme();
+
+  return (
+    <header className="sticky top-0 z-50 w-full bg-background">
+      <div className="container mx-auto flex h-16 items-center md:justify-between px-4 justify-end">
+
+        {/* Desktop Navigation */}
+        <section className="hidden md:flex items-center gap-6 w-full justify-center">
+          <Link href="/products" className="text-sm font-medium hover:text-primary">
+            work
+          </Link>
+          <Link href="/categories" className="text-sm font-medium hover:text-primary">
+            skills
+          </Link>
+          <Link href="/offers" className="text-sm font-medium hover:text-primary">
+            experience
+          </Link>
+          <Link href="/contact us" className="text-sm font-medium hover:text-primary">
+            testimonials
+          </Link>
+        </section>
+
+        {/* Actions */}
+        <div className="flex items-center gap-2">
+
+          {/* Theme */}
+          <div className='relative'>
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="ghost" size="icon">
+                        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                        <span className="sr-only">Toggle theme</span>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuItem onClick={() => setTheme("light")}>
+                        Light
+                      </DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        Dark
+                      </DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => setTheme("system")}>
+                        System
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+
+          {/* Mobile Menu */}
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="ghost" size="icon" className="md:hidden">
+                <Menu className="h-5 w-5" />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent side="right">
+              <section className="flex flex-col gap-4 mt-6 p-4">
+                <Link href="/products">work</Link>
+                <Link href="/categories">skills</Link>
+                <Link href="/offers">experience</Link>
+                <Link href="/offers">testimonials</Link>
+              </section>
+            </SheetContent>
+          </Sheet>
+
+        </div>
+      </div>
+    </header>
+  );
+}
