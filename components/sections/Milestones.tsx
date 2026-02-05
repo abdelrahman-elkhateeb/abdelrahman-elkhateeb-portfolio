f6ff383ab1cc3770ebc156bb7f6d3208024e00f5
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-02-05 23:27:38 +0200

    Update Milestones.tsx

--- components/sections/Milestones.tsx
+++ components/sections/Milestones.tsx
@@ -4,7 +4,7 @@ import SectionHeading from "../TypeWriterAnimation";
 export default function Milestones() {
   return (
     <section className="container mx-auto px-4 " id="work">
-      <SectionHeading text="milestone" />
+      <SectionHeading text="Milestone" tagType="h2" />
       <div className="flex gap-5">
         <MilestonesCard />
       </div>
