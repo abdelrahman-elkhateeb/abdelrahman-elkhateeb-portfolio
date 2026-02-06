9a4fa467ed754ec586d7929750419957c23732e9
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-02-06 13:54:38 +0200

    Update Hero.tsx

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -6,7 +6,7 @@ import { Button } from "../ui/button"
 
 export default function Hero() {
   return (
-    <section className="hero-gaming-bg h-[90vh]">
+    <section className="hero-gaming-bg h-[95vh]">
       <div className="relative isolate flex flex-col-reverse xl:flex-row items-center container mx-auto px-4 h-full">
         {/* hero text */}
         <div className="flex flex-col justify-center flex-1">
